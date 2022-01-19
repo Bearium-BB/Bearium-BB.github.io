905cf3242c5063dd5cf17475b915631f7261d214
Brett <58530824+timelord200410@users.noreply.github.com>
2022-01-19 14:56:05 -0600

    done

--- BasicsForWebDevelopment/Assignment/Calculator/index.js
+++ BasicsForWebDevelopment/Assignment/Calculator/index.js
@@ -4,15 +4,23 @@ const numberScreen = document.getElementById("numberScreen")
 var equation
 for (const i of allEntities) {
     i.addEventListener('click', function(event) {
-        if (i.value !== "=") {
-            equation = numberScreen.value += i.value
+
+        if (i.value !== "=" && i.value !== "CA") {
+            equation = numberScreen.value += i.value;
         }
+
         if (i.value == "CA") {
-            equation = numberScreen.value = ""
+            equation = numberScreen.value = "";
         }
-        if (i.value === "=") {
-            equation = numberScreen.value
-            numberScreen.value = eval(equation)
+
+        if (i.value === "=" && i.value === "") {
+
+            equation = numberScreen.value;
+
+            numberScreen.value = eval(equation);
+        } else {
+            equation = "";
+            numberScreen.value = "Error";
         }
     })
 }
