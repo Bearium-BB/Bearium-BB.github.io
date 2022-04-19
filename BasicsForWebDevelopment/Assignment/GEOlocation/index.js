302e3cb9595d4f5483054492cea2ceb922deb234
Brett <58530824+timelord200410@users.noreply.github.com>
2022-04-19 13:53:56 -0500

    done

--- BasicsForWebDevelopment/Assignment/GEOlocation/index.js
+++ BasicsForWebDevelopment/Assignment/GEOlocation/index.js
@@ -19,3 +19,14 @@ if (navigator.geolocation) {
 } else {
     console.log('Geolocation is not supported by your browser');
 }
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
